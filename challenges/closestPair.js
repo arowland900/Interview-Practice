346277a501675d0d87a3b392ae461b6ee2c66de2
Alexander Rowland <arowland900@gmail.com>
2021-06-17 00:28:53 -0700

    add best solution for closestPair

--- challenges/closestPair.js
+++ challenges/closestPair.js
@@ -28,3 +28,31 @@ function closestPair(target, leftNums, rightNums) {
         j % 2 ? target += j : target -= j
     }
 }
+
+
+// Best Solution
+// Time Complexity: O(n + m)
+function closestPair(target, leftNums, rightNums) {
+    leftNums = Array.from(new Set(leftNums)).sort((a, b) => b - a)
+    rightNums = Array.from(new Set(rightNums)).sort((a, b) => a - b)
+
+    let bestPair = [leftNums[0], rightNums[0]];
+    let bestPairValue = bestPair[0] + bestPair[1];
+    let i = 0, j = 0
+
+    while (i < leftNums.length && j < rightNums.length) {
+        let newPair = [leftNums[i], rightNums[j]]
+        let value = newPair[0] + newPair[1]
+
+        if (value == target) return newPair
+        else if (value < target) j++
+        else i++
+
+        if (Math.abs(value - target) < Math.abs(bestPairValue - target)) {
+            bestPair = newPair
+            bestPairValue = value
+        }
+        console.log(newPair, bestPair)
+    }
+    return bestPair
+}
