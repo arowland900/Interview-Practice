0c2b4146a3322b562934b582aa8c7e3584c98cba
Alexander Rowland <arowland900@gmail.com>
2021-06-21 23:04:36 -0700

    add twoSum

---
+++ challenges/twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+    let map = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        let c = target - nums[i]
+
+        if (map.has(c)) return [map.get(c), i]
+        map.set(nums[i], i)
+    }
+
+};
