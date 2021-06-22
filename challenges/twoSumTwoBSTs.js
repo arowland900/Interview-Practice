28d5db38a45b2c932fd2424dc60a831f03f3bd21
Alexander Rowland <arowland900@gmail.com>
2021-06-22 02:22:23 -0700

    add twoSumBSTs optimal solution

--- challenges/twoSumTwoBSTs.js
+++ challenges/twoSumTwoBSTs.js
@@ -1,3 +1,4 @@
+// Solution #1
 var twoSumBSTs = function (root1, root2, target) {
     let map1 = new Map()
     let map2 = new Map()
@@ -16,4 +17,29 @@ var twoSumBSTs = function (root1, root2, target) {
         if (map2.has(comb)) return true
     }
     return false
+};
+
+// Solution #2
+var twoSumBSTs = function (root1, root2, target) {
+    let map1 = new Map()
+    let answer = false
+    function bstToArr(node, map) {
+        map.set(target - node.val)
+        if (node.left) bstToArr(node.left, map)
+        if (node.right) bstToArr(node.right, map)
+    }
+    function traverseCheck(node) {
+        if (node) {
+            if (map1.has(node.val)) {
+                answer = true
+                return true
+            }
+            if (node.left) traverseCheck(node.left)
+            if (node.right) traverseCheck(node.right)
+        }
+    }
+
+    bstToArr(root1, map1)
+    traverseCheck(root2)
+    return answer
 };
